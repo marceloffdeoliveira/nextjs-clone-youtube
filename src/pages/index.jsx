76dd39233358c609ee228a04b6f5d7b0769170b8
Marcelo Francisco <marceloffdeoliveira2@gmail.com>
2021-02-13 12:21:54 -0300

    Video Component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,67 @@
-import { Button } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 import React from 'react';
 
-import Layout from '../components/Layout';
+import Layout from 'src/components/Layout';
+import VideoCard from 'src/components/VideoCard';
 
-export default function Home() {
+function Home({ data }) {
   return (
     <Layout title="Youtube">
-      <Button variant="outlined" color="secondary">
-        Teste
-      </Button>
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item.id} item xl={3} lg={3} md={3} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title: 'NOME DO VÍDEO',
+      authorId: 1,
+      authorName: 'Marcelo Francisco',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      title: 'NOME DO VÍDEO',
+      authorId: 2,
+      authorName: 'Marcelo Francisco',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      title: 'NOME DO VÍDEO',
+      authorId: 3,
+      authorName: 'Marcelo Francisco',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+  ];
+
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    },
+  };
+}
+
+export default Home;
